cd1d68c1a40c29805318787bbcf1a093fc242974
rohit94875 <59828158+rohit94875@users.noreply.github.com>
2020-01-29 11:41:15 +0530

    secondCommit

---
+++ app/src/main/java/com/coviam/quizapp/activity/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.coviam.quizapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coviam.quizapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
